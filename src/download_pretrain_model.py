83f5c029a6bc633bcbee452cf66c1527150f2c82
John Li <dhzhd1@gmail.com>
2017-08-23 15:57:26 -0700

    MxNet resnet-101 pretrain model downloader.

--- src/download_pretrain_model.py
+++ src/download_pretrain_model.py
@@ -12,13 +12,16 @@ def download_pretrain_model():
         print("Downloading {} and saving it under {}".format(model_file_name, pretrain_model_path))
         data_file.retrieve(model_url, os.path.join(pretrain_model_path, model_file_name))
         print("Model saved under {}".format(pretrain_model_path))
+    else:
+        print("Pretrain model {} existed!".format(model_file_name))
 
     if not os.path.isfile(os.path.join(pretrain_model_path, net_desc_file_name)):
         data_file = urllib.URLopener()
         print("Downloading {} and saving it under {}".format(model_file_name, pretrain_model_path))
         data_file.retrieve(net_desc_url, os.path.join(pretrain_model_path, net_desc_file_name))
         print("Net description file saved under {}".format(pretrain_model_path))
-
+    else:
+        print("Net description file {} existed!".format(net_desc_file_name))
 
 if __name__=='__main__':
     download_pretrain_model()
